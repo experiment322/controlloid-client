a7d493d80692f71936e71ccd913b6e5e981efc8a
Alexandru Damian Manea <maneaalexdamian@gmail.com>
2019-03-08 03:22:11 +0200

    Prepare project for signed release and add versioning support
    - add necessary configuration to app/build.gradle to support generating
    a signed release following the official tutorial
    - update jsc-android; (!) NOTE: there is a bug where the new jsc files
    are bundled only for armeabi-v7a and x86; this happens because for
    the other architectures the jsc .so files are not in
    org.webkit.android-jsc dependency but in the react-native-X.X.X.aar
    which results in duplicate .so files and because we use pickFirst in
    build.gradle the old .so files are used instead and the new ones are
    ignored; should wait for RN 0.59
    - add some instructions on the Home screen
    - use react-native-version to version the whole app (gradle,
    package.json, git tags) using a single command: npm version

--- js/interface/screens/HomeScreen.jsx
+++ js/interface/screens/HomeScreen.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { Surface } from 'react-native-paper';
+import { Linking, ScrollView } from 'react-native';
+import { Surface, Text } from 'react-native-paper';
 import Styles from '../styles';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class HomeScreen extends React.Component {
   render() {
     return (
-      <Surface style={Styles.screen} />
+      <Surface style={Styles.screen}>
+        <ScrollView>
+          <Text>
+            {
+              '\nWelcome to the beta version of Controlloid!\n\n\n'
+              + 'This application allows you to use your phone as a real controller:\n\n'
+              + '1. Go to Layouts screen to create, edit and star a layout. \n(you can drag around and resize the controls)\n\n'
+              + '2. Start the server on your PC and make sure that the phone and the computer are on the same network for optimal performance. \n(through WiFi or USB tethering)\n\n'
+              + '3. Go to Controller screen, write the IP address of your PC and press CONNECT. \n(be sure to begin the address with http://)\n\n'
+              + '4. To change the theme or tweak the controls go to Preferences screen. \n\n'
+              + '5. Done!\n\n\n'
+              + 'Visit the link below to download the server application.\n'
+              + 'To start the server run: \nLinux: "./dist/linux/start.sh"\nWindows: "./dist/windows/start.ps1"\n\n\n'
+            }
+          </Text>
+          <Text
+            style={{ color: 'blue' }}
+            onPress={() => Linking.openURL('https://github.com/experiment322/controlloid-server')}
+          >
+            {'https://github.com/experiment322/controlloid-server\n\n'}
+          </Text>
+        </ScrollView>
+      </Surface>
     );
   }
 }
